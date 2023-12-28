8d7671ae75adba3d0d3723d8fa069bc42ed06bb1
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2023-12-28 13:19:42 +0100

    objectify and exception handling

--- main.cpp
+++ main.cpp
@@ -14,91 +14,128 @@
     6. Functions | DONE
     7. Input | DONE
     8. Date | DONE
+    9. Exception handling | DONE
 
     1. Comments in english
-    2. Exception handling
 */
+
 void getDate() {
     time_t now = time(0);
     tm *ltm = localtime(&now);
     std::cout<<"Dzisiejsza data to: "<< ltm->tm_mday << "." << 1 + ltm->tm_mon << "." << 1900 + ltm->tm_year << "\n";
 }
 
-bool getUserSex() {
-    std::string userSex;
-    std::array<std::string, 10> female = {"k", "d", "g", "w", "kobieta", "dziewczyna", "female", "girl", "women", "woman"};
-    std::array<std::string, 10> male = {"m", "c", "ch", "mezczyzna", "facet", "chlopak", "male", "boy", "man", "men"};
+class User {
+    bool _sex;
+    int _age;
+    int _height;
+    int _mass;
+
+    bool _SetUserSex(){
+        std::string sexInput;
+        std::array<std::string, 4> female = {"k", "f", "kobieta", "female"};
+        std::array<std::string, 3> male = {"m", "mezczyzna", "male"};
+        
+        std::cout<<"Podaj swoja plec (wpisz h zeby zobaczyc liste akceptowanych wartosci):\n";
+        std::cin>>sexInput;
+        std::transform(sexInput.begin(), sexInput.end(), sexInput.begin(), ::tolower);
+        
+        if(sexInput == "h"){
+            std::cout<<"Akceptowane wartosci dla plci - kobieta:\n";
+            for(const std::string& value : female) std::cout<<"- "<<value<<"\n";
+            std::cout<<"Akceptowane wartosci dla plci - mezczyzna:\n";
+            for(const std::string& value : male) std::cout<<"- "<<value<<"\n";
+            return _SetUserSex();
+        }
+        else if(std::find(female.begin(), female.end(), sexInput) != female.end()) return true;
+        else if(std::find(male.begin(), male.end(), sexInput) != male.end()) return false;
+        else {
+            throw std::invalid_argument("Niepoprawna plec, sprobuj ponownie.\n");
+        }
+    }
+
+    int _SetUserAge(){
+        int ageInput;
+        std::cout<<"Podaj swoj wiek:\n";
+        std::cin>>ageInput;
+        if(ageInput < 5 || ageInput > 149) {
+            throw std::invalid_argument("Niepoprawny wiek, wiek powinien byc mniejszy niz 150 i nie mniejszy niz 5 sprobuj ponownie.\n");
+        }
+        else return ageInput;
+    }
+
+    int _SetUserHeight(){
+        int heightInput;
+        std::cout<<"Podaj swoj wzrost w centymetrach:\n";
+        std::cin>>heightInput;
+        if(heightInput < 20 || heightInput > 300) {
+            throw std::invalid_argument("Niepoprawny wzrost, wzrost powinien byc wiekszy badz rowny 20 cm i mniejszy badz rowny 300 cm. Sprobuj ponownie.\n");
+        }
+        else return heightInput;
+    }
     
-    do {
-        std::cout << "Podaj swoja plec (wpisz h zeby zobaczyc liste akceptowanych wartosci):\n";
-        std::cin >> userSex;
-        std::transform(userSex.begin(), userSex.end(), userSex.begin(), ::tolower);
-        if (userSex == "h"){
-            std::cout << "Akceptowane wartosci dla plci - kobieta:\n";
-            for (const std::string& value : female) std::cout << "- " << value << "\n";
-            std::cout << "Akceptowane wartosci dla plci - mezczyzna:\n";
-            for (const std::string& value : male) std::cout << "- " << value << "\n";
+    int _SetUserMass(){
+        int massInput;
+        std::cout<<"Podaj mase swojego ciala w kg:\n";
+        std::cin>>massInput;
+        if(massInput <= 0) {
+            throw std::invalid_argument("Niepoprawna masa, masa powinna byc wieksza niz 0. Sprobuj ponownie.\n");
         }
-        else if (std::find(female.begin(), female.end(), userSex) != female.end()) return true;
-        else if (std::find(male.begin(), male.end(), userSex) != male.end()) return false;
-        else std::cout << "Niepoprawna plec, sprobuj ponownie.\n";
-    } while (true);
-}
-
-int getUserAge() {
-    int age;
-    do {
-        std::cout << "Podaj swoj wiek:\n";
-        std::cin >> age;
-
-        if (age < 5 || age > 149) {
-            std::cout << "Niepoprawny wiek, wiek powinien byc mniejszy niz 150 i wiekszy niz 4 spróbuj ponownie.\n";
-        } else {
-            return age * 5;
+        else return massInput;
+    }
+    
+    public:
+        User(){
+            bool isSexSet = false;
+            while(!isSexSet){
+                try
+                { 
+                    _sex = _SetUserSex();
+                    isSexSet = true;
+                }
+                catch(const std::invalid_argument error_message){ std::cerr << error_message.what() << '\n'; }
+            }
+            bool isAgeSet = false;
+            while(!isAgeSet){
+                try
+                {
+                    _age = _SetUserAge();
+                    isAgeSet = true;
+                }
+                catch(const std::invalid_argument error_message){ std::cerr << error_message.what() << '\n'; }
+            }
+            bool isHeightSet = false;
+            while(!isHeightSet){
+                try
+                {
+                    _height = _SetUserHeight();
+                    isHeightSet = true;
+                }
+                catch(const std::invalid_argument error_message){ std::cerr << error_message.what() << '\n'; }
+            }
+            bool isMassSet = false;
+            while(!isMassSet){
+                try
+                {
+                    _mass = _SetUserMass();
+                    isMassSet = true;
+                }
+                catch(const std::invalid_argument error_message){ std::cerr << error_message.what() << '\n'; }
+            }
         }
-    } while (true);
-}
+        void CalculateBMR(){
+            int s;
+            if(_sex) s = -161;
+            else s = 5;
+            double bmr = _mass * 10 + _height * 6.25 - _age * 5 + s;
+            std::cout << "Twoje BMR wynosi " << bmr << " kcal.\n";
+        }
+};
 
-int getUserHeight() {
-    int height;
-    do {
-        std::cout << "Podaj swoj wzrost w centymetrach:\n";
-        std::cin >> height;
-
-        if (height < 20 || height > 300) std::cout << "Niepoprawny wzrost, wzrost powinien byc wiekszy badz rowny 20 cm i mniejszy badz rowny 300 cm. Sprobuj ponownie.\n";
-        else return height * 6.25;
-    } while (true);
-}
-
-int getUserMass() {
-    int mass;
-    do {
-        std::cout << "Podaj mase swojego ciala w kg:\n";
-        std::cin >> mass;
-
-        if (mass <= 0) std::cout << "Niepoprawna masa, masa powinna byc wieksza niz 0. Sprobuj ponownie.\n";
-        else return mass * 10;
-    } while (true);
-}
-
-double calculateBMR(bool userSex, int userAge, int userHeight, int userMass) {
-    int s; // This variable is named s in Mifflin St Jeor equation
-    if (userSex) s = -161; // Female
-    else s = 5; // Male
-    return userMass + userHeight - userAge + s;
-}
-
-int main() {
-    std::cout << "Witaj w kalkulatorze zapotrzebowania kalorycznego. Dzisiaj jest";
+int main(){
+    std::cout<<"Witaj w kalkulatorze BMR!\n";
     getDate();
-    
-    bool userSex = getUserSex();
-    int userAge = getUserAge();
-    int userHeight = getUserHeight();
-    int userMass = getUserMass();
-    double userBMR = calculateBMR(userSex, userAge, userHeight, userMass);
-
-    std::cout << "Twoje BMR wynosi "<< userBMR <<" kcal.\n";
-
+    User user;
+    user.CalculateBMR();
     return 0;
 }
